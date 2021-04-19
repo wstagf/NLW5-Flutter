590d551760a5e2f8bd41741c7da1e8a6e6b0dd2d
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-19 15:15:48 -0300

    colocado appbar

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'widget/app_bar_widget.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
   }
 }
