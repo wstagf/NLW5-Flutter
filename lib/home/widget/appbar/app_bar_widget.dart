602d0a804b221ed0423771b3fd5ec7091c6887e0
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-19 17:06:36 -0300

    finalizado layout da appbar

--- lib/home/widget/appbar/app_bar_widget.dart
+++ lib/home/widget/appbar/app_bar_widget.dart
@@ -9,44 +9,57 @@ class AppBarWidget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(gradient: AppGradients.linear),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text.rich(
-                        TextSpan(
-                          text: 'Olá, ',
-                          style: AppTextStyles.title,
-                          children: [
-                            TextSpan(
-                              text: 'Thiago',
-                              style: AppTextStyles.titleBold,
-                            ),
-                          ],
-                        ),
+            child: Stack(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      height: 171,
+                      padding: const EdgeInsets.only(
+                        top: 0,
+                        left: 20,
+                        bottom: 20,
+                        right: 20,
                       ),
-                      Container(
-                        width: 58,
-                        height: 58,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                            image: NetworkImage(
-                              'https://pt.gravatar.com/userimage/146224715/8bf33285aa3f3a7b1a0906865b2d70ec.png',
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(gradient: AppGradients.linear),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text.rich(
+                            TextSpan(
+                              text: 'Olá, ',
+                              style: AppTextStyles.title,
+                              children: [
+                                TextSpan(
+                                  text: 'Thiago',
+                                  style: AppTextStyles.titleBold,
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                  ScoreCardWidget(),
-                ],
-              ),
+                          Container(
+                            width: 58,
+                            height: 58,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  'https://pt.gravatar.com/userimage/146224715/8bf33285aa3f3a7b1a0906865b2d70ec.png',
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
+              ],
             ),
           ),
         );
--- lib/home/widget/score_card/score_card_widget.dart
+++ lib/home/widget/score_card/score_card_widget.dart
@@ -5,43 +5,51 @@ import 'package:flutter/material.dart';
 class ScoreCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 136,
-      decoration: BoxDecoration(
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              width: 80,
-              height: 80,
-              child: ChartWidget(),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 24),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Vamos Começar',
-                    style: AppTextStyles.heading,
-                  ),
-                  Text(
-                    'Complete os desafios e ganhe conhecimento',
-                    style: AppTextStyles.body,
-                  ),
-                ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 0.6,
+              )
+            ]),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: 80,
+                height: 80,
+                child: ChartWidget(),
               ),
             ),
-          )
-        ],
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Vamos Começar',
+                      style: AppTextStyles.heading,
+                    ),
+                    Text(
+                      'Complete os desafios e ganhe conhecimento',
+                      style: AppTextStyles.body,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
