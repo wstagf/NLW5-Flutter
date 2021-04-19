cb0800884f66f1129d68b03c1ecac4da8ca1d4ab
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-19 16:02:33 -0300

    finalizado estilizacao do card

--- lib/home/widget/score_card/score_card_widget.dart
+++ lib/home/widget/score_card/score_card_widget.dart
@@ -1,9 +1,16 @@
+import 'package:devquizflutter/core/core.dart';
+import 'package:devquizflutter/home/widget/chart_widget/chart_widget.dart';
 import 'package:flutter/material.dart';
 
 class ScoreCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 136,
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Row(
         children: [
           Expanded(
@@ -11,7 +18,7 @@ class ScoreCardWidget extends StatelessWidget {
             child: Container(
               width: 80,
               height: 80,
-              color: Colors.red,
+              child: ChartWidget(),
             ),
           ),
           Expanded(
@@ -19,9 +26,17 @@ class ScoreCardWidget extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.only(left: 24),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text('Vamos Começar'),
-                  Text('Complete os desafios e ganhe conhecimento'),
+                  Text(
+                    'Vamos Começar',
+                    style: AppTextStyles.heading,
+                  ),
+                  Text(
+                    'Complete os desafios e ganhe conhecimento',
+                    style: AppTextStyles.body,
+                  ),
                 ],
               ),
             ),
