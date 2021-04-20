b1a94f690a74f3846a0d79bbe181610b98eed08a
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-20 14:54:08 -0300

    criado indicador de questao

--- lib/challenge/widget/question_indicator_widget/question_indicator_widget.dart
+++ lib/challenge/widget/question_indicator_widget/question_indicator_widget.dart
@@ -1,22 +1,27 @@
+import 'package:devquizflutter/shared/widgets/custom_progress_indicator/custom_progress_indicator.dart';
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Column(
+          children: [
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text("Questão 04"),
                 Text("de 10"),
               ],
             ),
-          ),
-        ],
+            SizedBox(
+              height: 16,
+            ),
+            CustomProgressIndicator(value: 0.7),
+          ],
+        ),
       ),
     );
   }
