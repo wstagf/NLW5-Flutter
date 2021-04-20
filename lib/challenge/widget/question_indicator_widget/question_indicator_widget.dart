21bedce505faea8116880c70d6ab397f1d82c64d
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-20 14:45:54 -0300

    criado widget questionIndicator

---
+++ lib/challenge/widget/question_indicator_widget/question_indicator_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Questão 04"),
+                Text("de 10"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
