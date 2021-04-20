2810f02be6c2a683efca1ffde8dc2123bc1a4b7e
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-20 15:49:54 -0300

    Criado widget Quizwidget

---
+++ lib/challenge/widget/quiz_widget/quiz_widget.dart
@@ -0,0 +1,25 @@
+import 'package:devquizflutter/core/core.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+
+  const QuizWidget({Key key, @required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(
+            height: 14,
+          ),
+        ],
+      ),
+    );
+  }
+}
